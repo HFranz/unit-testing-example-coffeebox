129811c5f9f3820c848a43c6cf6787f8ba8afe60
Julian Schaefer <schaefer.julian@gmail.com>
2016-09-07 21:45:21 +0200

    Added failing test

--- demo-application/src/test/java/KaffeekasseTest.java
+++ demo-application/src/test/java/KaffeekasseTest.java
@@ -1,11 +1,18 @@
+import static org.junit.Assert.*;
 
+import org.junit.Test;
 
 public class KaffeekasseTest {
 
-  @Test
-  public void testAbrechnung() {
-      Kaffeekasse kk = new Kaffeekasse();
+	@Test
+	public void testAbrechnung() {
+		Kaffeekasse kk = new Kaffeekasse();
 
-      assertTrue(kk.erstelleAbrechnung() == 0);
-  }
+		assertTrue(kk.erstelleAbrechnung() == 0);
+	}
+
+	@Test
+	public void failingTest() throws Exception {
+		assertTrue(false);
+	}
 }
